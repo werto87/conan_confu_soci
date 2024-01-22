ab8382ff0910339f193bbfebc7d04d2199672cc1
werto87 <huhulasmichinruhe@web.de>
2024-01-23 08:14:30 +0900

    wip conan2

--- all/conanfile.py
+++ all/conanfile.py
@@ -1,7 +1,14 @@
-import shutil
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc
+from conan.tools.scm import Version
 import os
-from conans.tools import download, unzip, check_md5, check_sha1, check_sha256
-from conans import ConanFile, CMake, tools
+
 
 
 class ConfuSociConan(ConanFile):
@@ -14,7 +21,8 @@ class ConfuSociConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    generators = "cmake"
+    generators = "CMakeToolchain"
+
 
     @property
     def _source_subfolder(self):
@@ -27,20 +35,22 @@ class ConfuSociConan(ConanFile):
     def configure(self):
         self.options["soci"].with_boost = True
         self.options["soci"].with_sqlite3 = True
+        self.options["soci"].shared = True
 
     def requirements(self):
-        self.requires("soci/4.0.2@werto87/stable")
+        self.requires("soci/4.0.3") #TODO find out why this can not be found. maybe open an issue on conan
         self.requires("magic_enum/[>=0.7.2]")
+        self.requires("boost/1.83.0")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder=self._source_subfolder)
+        cmake.configure()
         cmake.build()
+
 
     def package(self):
         self.copy("*.h*", dst="include/confu_soci",
@@ -52,3 +62,4 @@ class ConfuSociConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = ["convenienceFunctionForSoci"]
+
